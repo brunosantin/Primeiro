8e6ebaf18d1b5f77c516830ad968ed2c4f0dfdcf
Bruno Santin <brsantin1989@gmail.com>
2020-07-25 16:15:05 -0300

    Primeiro v1

---
+++ Primeiro/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace Primeiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Produto p = new Produto("TV", 100.00, 10);
+
+            p.Nome = "T";
+
+            Console.WriteLine(p.Nome);
+
+
+           /* Console.WriteLine("Entre oos dados do produto: ");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            Produto p = new Produto(nome, preco);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do Produto: " + p);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite a entrada de estoque : ");
+            int qte = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+            p.AdicionarProdutos(qte);
+            Console.WriteLine("Dados do Produto: " + p);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite a Saida de estoque : ");
+            qte = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+            p.RemoverProdutos(qte);
+            Console.WriteLine("Dados do Produto: " + p); */
+        }
+    }
+} 
